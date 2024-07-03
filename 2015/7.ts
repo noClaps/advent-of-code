147ee950180400ca2b69e17db6ccb1ef54e8d2a0
noClaps <github@zerolimits.dev>
2024-07-03 18:17:04 +0100

    Remove leftover comments

--- 2015/7.ts
+++ 2015/7.ts
@@ -1,14 +1,4 @@
 const input = await Bun.file("7.txt").text();
-// const input = `
-// 123 -> x
-// 456 -> y
-// x AND y -> d
-// x OR y -> e
-// x LSHIFT 2 -> f
-// y RSHIFT 2 -> g
-// NOT x -> h
-// NOT y -> i
-// `.trim();
 
 let lines = input.split("\n");
 
